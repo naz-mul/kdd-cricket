bad9a20ae32cd0951612cb24f47b51e903252724
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2017-04-23 00:57:46 +0100

    pre-processing: gender column added;

--- CRICKET.R
+++ CRICKET.R
@@ -4,12 +4,16 @@
 
 # Define required packages and install new packages
 list.of.packages <- c("jsonlite", "lubridate")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+new.packages <-
+  list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
+if (length(new.packages))
+  install.packages(new.packages)
 
 
+# Set working directory and import raw data
 setwd("~/Desktop/kdd-cricket")
 raw.data <- read.csv("2-target-data/cricinfo_odi_data.csv")
+
 
 # find the date range of the cricket matches
 library(lubridate)
@@ -18,21 +22,31 @@ raw.data$date <- ymd(as.character(raw.data$date))
 summary(raw.data$date)
 
 # Find the missing odi matches
+# The missing matches are still to be played as of 23rd April 2017
 library(jsonlite)
 json <- fromJSON("1-data-sources/odi.json")
 json <- as.vector(as.integer(json))
-ids <- raw.data$id
-ids <- as.vector(ids)
-
-setdiff(json, ids)
+setdiff(json, raw.data$id)
 
 # find the number of duplicated ids
 dim(raw.data[duplicated(raw.data$id),])[1]
 
 
 # Create a new column for gender
-raw.data$gender <- "male"
+raw.data$gender <- NA
+raw.data$gender <-
+  mapply(grepl,
+         pattern = "Women",
+         x = raw.data$team_a)
 
-raw.data$gender <- mapply(grepl, pattern="Women", x= raw.data$team_a)
+set.gender = function(gendercol) {
+  for (i in 1:length(gendercol)) {
+    if (gendercol[i] == TRUE)
+      gendercol[i] <- "female"
+    else
+      gendercol[i] <- "male"
+  }
+  gendercol
+}
 
-
+raw.data$gender <- set.gender(raw.data$gender)
