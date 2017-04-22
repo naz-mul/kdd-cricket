38a966952c52379d8e3ba93d32bdb937a30cb241
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2017-04-22 22:36:36 +0100

    added reference files

--- CRICKET.R
+++ CRICKET.R
@@ -1,14 +1,25 @@
 # Created by: nazmul
 # Created on: 21/04/17
+# http://www.cricketabstract.com/
 
-install.packages("jsonlite")
-library(jsonlite)
+# Define required packages and install new packages
+list.of.packages <- c("jsonlite", "lubridate")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 
 setwd("~/Desktop/kdd-cricket")
-raw.data <- read.csv("final_output.csv")
+raw.data <- read.csv("2-target-data/cricinfo_odi_data.csv")
+
+# find the date range of the cricket matches
+library(lubridate)
+sapply(raw.data$date, class)
+raw.data$date <- ymd(as.character(raw.data$date))
+summary(raw.data$date)
 
 # Find the missing odi matches
-json <- fromJSON("matches-odi.json")
+library(jsonlite)
+json <- fromJSON("1-data-sources/odi.json")
 json <- as.vector(as.integer(json))
 ids <- raw.data$id
 ids <- as.vector(ids)
