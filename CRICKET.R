f38fc34881dda17bdc126db98cc2acace5e6ba21
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2017-05-01 02:19:53 +0100

    finished data exploration

--- CRICKET.R
+++ CRICKET.R
@@ -1,5 +1,9 @@
 # Define required packages and install new packages
-dm.required.packages <- c("lubridate", "dplyr", "ggplot2", "scales")
+dm.required.packages <-
+  c("lubridate",
+    "dplyr",
+    "ggplot2",
+    "scales")
 
 package.install.func <- function(x) {
   for (i in x) {
@@ -67,48 +71,108 @@ boxplot(first_innings_total ~ gender,
 
 # Proportion of tosses won in home country
 # Proportion of tosses won when playing away from home
-homet <- with(cricket[cricket$team_a_venue == "home",], xtabs( ~ team_a_venue + toss_decision))
-homet/rowSums(homet) # 53% bat, 47% bowl
-awayt <- with(cricket[cricket$team_a_venue == "away",], xtabs( ~ team_a_venue + toss_decision))
-awayt/rowSums(awayt) # 55% bat, 45% bowl
+homet <-
+  with(cricket[cricket$team_a_venue == "home", ], xtabs(~ team_a_venue + toss_decision))
+homet / rowSums(homet) # 53% bat, 47% bowl
+binom.test(1195, 2258, p = .5)
+
+awayt <-
+  with(cricket[cricket$team_a_venue == "away", ], xtabs(~ team_a_venue + toss_decision))
+awayt / rowSums(awayt) # 55% bat, 45% bowl
 
 # Proportion of toss decision when played home and D/N match
-homet.dn <- with(cricket[cricket$team_a_venue == "home" & cricket$day_night == 1,], xtabs( ~ team_a_venue + toss_decision))
-homet.dn/rowSums(homet.dn) # 72% bat, 28% bowl
-awayt.dn <- with(cricket[cricket$team_a_venue == "away" & cricket$day_night == 0,], xtabs( ~ team_a_venue + toss_decision))
-awayt.dn/rowSums(awayt.dn) # 47% bat, 53% bowl
+homet.dn <-
+  with(cricket[cricket$team_a_venue == "home" &
+                 cricket$day_night == 1, ], xtabs(~ team_a_venue + toss_decision))
+homet.dn / rowSums(homet.dn) # 72% bat, 28% bowl
+awayt.dn <-
+  with(cricket[cricket$team_a_venue == "away" &
+                 cricket$day_night == 0, ], xtabs(~ team_a_venue + toss_decision))
+awayt.dn / rowSums(awayt.dn) # 47% bat, 53% bowl
 
 
 # Proportion of toss decision when played home and rain
-homet.r <- with(cricket[cricket$team_a_venue == "home" & cricket$rain == 1,], xtabs( ~ team_a_venue + toss_decision))
-homet.r/rowSums(homet.r) # 43% bat, 57% bowl
-awayt.r <- with(cricket[cricket$team_a_venue == "away" & cricket$rain == 0,], xtabs( ~ team_a_venue + toss_decision))
-awayt.r/rowSums(awayt.r) # 55% bat, 45% bowl
+homet.r <-
+  with(cricket[cricket$team_a_venue == "home" &
+                 cricket$rain == 1, ], xtabs(~ team_a_venue + toss_decision))
+homet.r / rowSums(homet.r) # 43% bat, 57% bowl
+awayt.r <-
+  with(cricket[cricket$team_a_venue == "away" &
+                 cricket$rain == 0, ], xtabs(~ team_a_venue + toss_decision))
+awayt.r / rowSums(awayt.r) # 55% bat, 45% bowl
 
 # Proportion of toss decision when played home, day night and rain
-homet.dn.r <- with(cricket[cricket$team_a_venue == "home" & cricket$day_night == 1 & cricket$rain == 1,], xtabs( ~ team_a_venue + toss_decision))
-homet.dn.r/rowSums(homet.dn.r) # 52% bat, 48% bowl
-awayt.dn.r <- with(cricket[cricket$team_a_venue == "away" & cricket$day_night == 0 & cricket$rain == 0,], xtabs( ~ team_a_venue + toss_decision))
-awayt.dn.r/rowSums(awayt.dn.r) # 47% bat, 53% bowl
+homet.dn.r <-
+  with(cricket[cricket$team_a_venue == "home" &
+                 cricket$day_night == 1 &
+                 cricket$rain == 1, ], xtabs(~ team_a_venue + toss_decision))
+homet.dn.r / rowSums(homet.dn.r) # 52% bat, 48% bowl
+awayt.dn.r <-
+  with(cricket[cricket$team_a_venue == "away" &
+                 cricket$day_night == 0 &
+                 cricket$rain == 0, ], xtabs(~ team_a_venue + toss_decision))
+awayt.dn.r / rowSums(awayt.dn.r) # 47% bat, 53% bowl
 
 
 
 # Win toss win game
+cricket$teama_win_toss <-
+  1 * (cricket$team_a == cricket$toss_winner)
+cricket$teamb_win_toss <-
+  1 * (cricket$team_b == cricket$toss_winner)
+cricket$teama_win_game <- 1 * (cricket$team_a == cricket$winner)
+cricket$teamb_win_game <- 1 * (cricket$team_b == cricket$winner)
+(cricket$win_toss_win_game <-
+    ((cricket$teama_win_toss &
+        cricket$teama_win_game == 1) |
+       (cricket$teama_win_toss & cricket$teamb_win_game == 1)
+    ))
 
+library(dplyr)
+cricket.toss <-
+  select(.data = cricket, team_a, team_b, winner, toss_winner, win_toss_win_game)
 
+# proportion of matches won
+prop.table(table(cricket.toss$win_toss_win_game)) # 49% do not win, 51% wins
 
+# proportion of matches won by gender
+prop.table(table(cricket$gender, cricket.toss$win_toss_win_game), 1)
 
+# proportion of matches won by countries
+prop <-
+  prop.table(table(cricket.toss$winner,
+                   cricket.toss$win_toss_win_game),
+             1) * 100
+prop
 
+barplot(
+  t(as.matrix(prop)),
+  width = 2,
+  beside = T,
+  space = c(0, 2),
+  ylim = c(0, 60),
+  main = "Win vs Lost % on winning the toss",
+  ylab = "% won",
+  col = c("black", "white")
+)
+legend(
+  "bottom",
+  legend = c("lost", "won"),
+  fill = c("black", "white"),
+  ncol = 2,
+  cex = 0.75
+)
 
 
 # Home match better probablity of winniing
 library(dplyr)
 sel_cricket <-
   select(.data = cricket, team_a, team_b, winner, toss_winner, team_a_venue)
-sel_cricket$team_a <- as.character(sel_cricket$team_a)
-sel_cricket$team_b <- as.character(sel_cricket$team_b)
-sel_cricket$winner <- as.character(sel_cricket$winner)
-sel_cricket$toss_winner <- as.character(sel_cricket$toss_winner)
+# sel_cricket$team_a <- as.character(sel_cricket$team_a)
+# sel_cricket$team_b <- as.character(sel_cricket$team_b)
+# sel_cricket$winner <- as.character(sel_cricket$winner)
+# sel_cricket$toss_winner <- as.character(sel_cricket$toss_winner)
+# sel_cricket$team_a_venue <- as.character(sel_cricket$team_a_venue)
 
 str(sel_cricket)
 
@@ -117,9 +181,8 @@ homewin <-
     sel_cricket %>%
       filter(team_a_venue == "home") %>%
       group_by(team_a, team_b) %>%
-      summarise(won = sum(team_a == winner), total = n()) %>%
-      mutate(percent = (won / total) * 100) %>%
-      arrange(team_a, team_b)
+      summarise(won = sum(team_a == winner), total = length(winner)) %>%
+      mutate(percent = (won / total) * 100) %>% arrange(team_a, team_b)
   )
 
 awaywin <-
@@ -127,9 +190,8 @@ awaywin <-
     sel_cricket %>%
       filter(team_a_venue == "away") %>%
       group_by(team_a, team_b) %>%
-      summarise(won = sum(team_a == winner), total = n()) %>%
-      mutate(percent = (won / total) * 100) %>%
-      arrange(team_a, team_b)
+      summarise(won = sum(team_a == winner), total = length(winner)) %>%
+      mutate(percent = (won / total) * 100)  %>% arrange(team_a, team_b)
   )
 
 splitted.data.home <- split(homewin, homewin$team_a)
@@ -139,7 +201,7 @@ par(mfrow = c(1, 1))
 
 library(ggplot2)
 library(scales)
-for (country in levels(cricket$team_a)) {
+for (country in levels(cricket$team_a)[1:9]) {
   chome <- splitted.data.home[country]
   coutside <- splitted.data.away[country]
   title <- paste(country, "vs Rest")
@@ -161,22 +223,25 @@ for (country in levels(cricket$team_a)) {
     )
   dat <- rbind(dat1, dat2)
   
-  p4 <- ggplot() + theme_bw() + geom_bar(aes(y = percent, x = opponent, fill = type),
-                                        data = dat,
-                                        stat = "identity")
+  p4 <-
+    ggplot() + theme_bw() + geom_bar(aes(y = percent, x = opponent, fill = type),
+                                     data = dat,
+                                     stat = "identity")
   p4 <-
     p4 + theme(axis.text.x = element_text(angle = 70, hjust = 1))
   
   p4 <-
-    p4 + geom_text(data = dat,
-                   aes(
-                     x = opponent,
-                     y = percent,
-                     label = paste0(round(percent,
-                                          2), "%")
-                   ),
-                   size = 3, 
-                   position = position_stack(vjust = 0.5))
+    p4 + geom_text(
+      data = dat,
+      aes(
+        x = opponent,
+        y = percent,
+        label = paste0(round(percent,
+                             2), "%")
+      ),
+      size = 3,
+      position = position_stack(vjust = 0.5)
+    )
   
   p4 <-
     p4 + theme(
@@ -222,13 +287,13 @@ mean(awaywin$percent[awaywin$team_a == "Zimbabwe"]) # NaN
 
 
 # 300 or more runs, more wins
-score300 <- with(cricket[cricket$first_innings_total >= 300,], xtabs( ~ first_innings_total, winner))
-score300/rowSums(score300) # 
-score300
-
-plot(score300)
-
+score300 <-
+  with(cricket[cricket$first_innings_total >= 300, ], xtabs(~ first_innings_total, winner))
+# score300 / rowSums(score300) #
+# score300
 
 
 
-###### Data Mining - Transformed & Pre-processed data #####
+
+
+###### Data Mining - Association Rules (apriori) #####
